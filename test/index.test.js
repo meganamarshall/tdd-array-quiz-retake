7062dd5e9bb5f91cc954bb7c94a6f6499c1e0bd2
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-12 15:13:52 -0700

    testing with console logs

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ const bigArray = require('../lib/index.js');
 describe('array function', () => {
   it('takes an array and modifies it', () => {
     const array = [1, 3, 5, 7, 2];
+    console.log(bigArray(array), 'expect');
     expect(bigArray(array)).toEqual(['Wow, 7 is big!']);
   });
 });
