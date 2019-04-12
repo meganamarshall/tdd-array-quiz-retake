4b6f06cec7f406d0884b37bd7f0402c46abb4665
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-12 15:15:28 -0700

    wrote second test to make sure it works

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,10 @@ const bigArray = require('../lib/index.js');
 describe('array function', () => {
   it('takes an array and modifies it', () => {
     const array = [1, 3, 5, 7, 2];
-    console.log(bigArray(array), 'expect');
     expect(bigArray(array)).toEqual(['Wow, 7 is big!']);
+  });
+  it('won\'t return anything if all numbers are 5 or less', () => {
+    const array = [4, 5, 3, 2];
+    expect(bigArray(array)).toEqual([]);
   });
 });
